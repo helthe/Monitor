208b77a3b5e2684728abddac14514bbcf7273808
Carl Alexander <carlalexander@gmail.com>
2014-02-17 23:25:06 -0500

    Missing LICENSE header

--- Tests/Fixture/Foo.php
+++ Tests/Fixture/Foo.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the Helthe Monitor package.
+ *
+ * (c) Carl Alexander <carlalexander@helthe.co>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Helthe\Monitor\Tests\Fixture;
 
 class Foo
